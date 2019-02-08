441a62867d1e8f0961159286ee5551ccf21238d0
William James <34557830+wlpjames@users.noreply.github.com>
2019-02-08 13:46:14 +0000

    Update saw.c

--- saw.c
+++ saw.c
@@ -1,34 +1,30 @@
+#include <stdlib.h>
+#include <math.h>
 
 //last at begining = 0
-void saw_tooth(float signal[], int siglen, float freq, int SR, int last)
+float saw_tooth(float signal[], int siglen, float freq, int SR, float last, float volume)
+{
+	
+	float wav_len = SR / freq;
+	float half_wave_len = wav_len/2;
+
+	for (int i = 0; i < siglen; i++){
+		signal[i] = (( fmod((float) i+last, wav_len) / half_wave_len) - 1) * volume;
+	}
+
+	return fmod((float) siglen + last, wav_len);
+
+}
+
+float tri_wave(float signal[], int siglen, float freq, int SR, float last, float volume)
 {
 	float wav_len = SR / freq;
 	float half_wave_len = wav_len/2;
 
-
-	for (int i = 0; i < siglen; i++){
-		signal[i] = (((i+last) % wav_len)/half_wave_len) - 1;
+	for (int i = 0; i < siglen; i++) {
+		signal[i] = (2 * fabs( (fmod((float) i+last, wav_len) / half_wave_len) - 1) - 1) * volume;
 	}
 
-	last = siglen % wavlen;
-
-	return;
+	return fmod((float) siglen + last, wav_len);
 
 }
-
-void tri_wave(float signal[], int siglen, float freq, int SR, int last)
-{
-	float wav_len = SR / freq;
-	float half_wave_len = wav_len/2;
-
-
-	for (int i = 0; i < siglen; i++){
-		signal[i] = 2 * abs((((i+last) % wav_len)/half_wave_len) - 1) - 1;
-	}
-
-	last = siglen % wavlen;
-
-	return;
-}
-
-void noise(float[] signal, siglen )
